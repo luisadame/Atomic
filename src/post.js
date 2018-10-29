624cbfbb7f90119df91c686145bad4d57004e4d0
Luis Adame <mrluissan@gmail.com>
2018-10-29 22:01:14 +0100

    Create basic post class

---
+++ src/post.js
@@ -0,0 +1,29 @@
+export default class Post {
+    constructor(title) {
+        this.title = title;
+    }
+
+    set title(title) {
+        this._title = title;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set content(content) {
+        this._content = content;
+    }
+
+    get content() {
+        return this._content;
+    }
+
+    set source(source) {
+        this._source = source;
+    }
+
+    get source() {
+        return this._source;
+    }
+}
