ca45c3328e987adc5008bf70c6657c65103b1c5f
Luis Adame <mrluissan@gmail.com>
2018-11-06 02:51:54 +0100

    Import sidebar, logo, categories, and run their startup methods

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import '../assets/css/app.scss';
+import '../assets/img/logo.svg';
 
 // database
 import repository from './repository.js';
 import Database from './database';
 import Post from './post';
+import Sidebar from './components/sidebar';
+import Category from './category';
 const db = new Database(repository);
 //eslint-disable-next-line no-console
 Post.render(db.posts);
+Category.render(db.categories);
+
+// we'll split this later
+Sidebar.listen();
