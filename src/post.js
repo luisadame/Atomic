c2a10bb14c58361791f8cf1445cc81143a3b2612
Luis Adame <mrluissan@gmail.com>
2018-11-07 21:39:50 +0100

    Add modal functionality to post, this will end up splitted into its file

--- src/post.js
+++ src/post.js
@@ -61,11 +61,65 @@ export default class Post {
 			<article class="post ${this.getPostSize()}">
 				<img class="post__img" src="${this.image}" alt="Article featured image">
 				<div class="post__content">
-					<h2 class="post__title">${this.title}</h2>
+					<h2 class="post__title">
+						<a href="/post/${this.title}">
+							${this.title}
+						</a>
+					</h2>
 					<p class="post__source">${this.source.title}</p>
 				</div>
 			</article>
 		`;
+	}
+
+	buildModal(rects) {
+		this.closing = false;
+		return `
+			<article class="post--modal" style="top: ${rects.top}px; left: ${
+	rects.left
+}px; width: ${rects.width}px; height: ${rects.height}px">
+				<button class="post--modal__back">Back</button>
+				<img class="post__img" src="${this.image}" alt="Article featured image">
+				<h2 class="post__title">${this.title}</h2>
+				<div class="post__content">${this.content}</div>
+				<p class="post__source">${this.source.title}</p>
+			</article>
+	`;
+	}
+
+	destroyModal() {
+		this.closing = true;
+		let back = document.querySelector('.post--modal .post--modal__back');
+		back.removeEventListener('click', this.destroyModal, false);
+		document.querySelector('.post--modal').classList.remove('active');
+		document.body.classList.remove('modal-opened');
+	}
+
+	listenModal() {
+		let modal = document.querySelector('.post--modal');
+		modal.addEventListener('transitionend', () => {
+			if (this.closing) modal.remove();
+		});
+		let back = document.querySelector('.post--modal .post--modal__back');
+		back.addEventListener('click', this.destroyModal.bind(this), false);
+	}
+
+	static getParent(e) {
+		let parent = e.target.parentElement;
+		while (!parent.classList.contains('post')) parent = parent.parentElement;
+		return parent;
+	}
+
+	static loadPost(e) {
+		e.preventDefault();
+		let post = db.post(e.target.textContent.trim());
+		let parentRect = Post.getParent(e).getBoundingClientRect();
+		document.body.insertAdjacentHTML('beforeend', post.buildModal(parentRect));
+		post.listenModal();
+		document.body.classList.add('modal-opened');
+		setTimeout(() => {
+			document.querySelector('.post--modal').classList.add('active');
+		}, 20);
 	}
 
 	static render(posts) {
@@ -73,5 +127,9 @@ export default class Post {
 		posts.forEach(post => fragment.push(post.render()));
 		let $posts = document.querySelector('.posts');
 		$posts.innerHTML = fragment.join('');
+		const postTitles = document.querySelectorAll('.post__title');
+		postTitles.forEach(title =>
+			title.addEventListener('click', Post.loadPost, false)
+		);
 	}
 }
