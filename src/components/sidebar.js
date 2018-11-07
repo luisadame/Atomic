b3a9e55ad3eac0dbfd9dee53759bbef002549288
Luis Adame <mrluissan@gmail.com>
2018-11-07 14:05:34 +0100

    Better handling of sidebar toggling

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,20 +1,22 @@
 export default class Sidebar {
 	constructor() {
 		this.opened = false;
-		this.el = document.querySelector('.sidebar');
+		this.header = document.querySelector('.header');
 		this.btn = document.querySelector('.logo');
-		this.toolbar = document.querySelector('.toolbar');
+		this.cloak = document.querySelector('.sidebar__cloak');
 	}
 
 	toggle() {
-		this.el.classList.toggle('opened');
-		this.btn.classList.toggle('fixed');
-		this.toolbar.classList.toggle('opened');
+		this.header.classList.toggle('sidebar--opened');
 		this.opened = !this.opened;
 	}
 
 	static listen() {
 		const sidebar = new Sidebar();
 		sidebar.btn.addEventListener('click', sidebar.toggle.bind(sidebar));
+		sidebar.cloak.addEventListener('click', sidebar.toggle.bind(sidebar));
+		window.addEventListener('keypress', key => {
+			if (key.key === 'Escape') sidebar.toggle.call(sidebar);
+		});
 	}
 }
