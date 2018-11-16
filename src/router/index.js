62a65bda1d5a82d5462bdd20326d82e678ab22ed
Luis Adame <mrluissan@gmail.com>
2018-11-16 23:14:07 +0100

    Set previous url and document title, and add back and home methods.

--- src/router/index.js
+++ src/router/index.js
@@ -39,13 +39,35 @@ export default class Router {
 	static async go(title, url) {
 		// first lets save the current url to localStorage
 		localStorage.setItem('previous_url', JSON.stringify(window.location));
+		localStorage.setItem('previous_name', document.title);
 		// set document title
 		document.title = title;
 		// then modify history
 		window.history.pushState({}, '', url);
 	}
 
+	/**
+	 * Navigate to the previous url if its different from home.
+	 */
+	static async back() {
+		let previous = JSON.parse(localStorage.getItem('previous_url'));
+		if (!previous || previous.hash === '' || previous.href === window.location.href) {
+			await Router.go(window.app.name, window.location.href.replace(/#.+/, ''));
+		} else {
+			await Router.go(localStorage.getItem('previous_name'), previous.href);
+		}
+	}
+
+	/**
+	 * Go to home page.
+	 */
+	static async home() {
+		await Router.go(window.app.name, window.location.href.replace(/#.+/, ''));
+	}
+
 	init() {
+		localStorage.removeItem('previous_url');
+		localStorage.removeItem('previous_name');
 		this.addListener();
 		this.handleCurrentRoute();
 	}
