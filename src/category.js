ae6a194256c049d17f31f36098ea33ba40e89fac
Luis Adame <mrluissan@gmail.com>
2018-11-16 23:15:17 +0100

    Add slug method, complete render method, add open and load category methods.

--- src/category.js
+++ src/category.js
@@ -1,3 +1,7 @@
+import Router from './router';
+import Post from './post';
+import Sidebar from './components/sidebar';
+
 export default class Category {
 	constructor(name) {
 		this.name = name;
@@ -20,13 +24,53 @@ export default class Category {
 		return this._sources;
 	}
 
+	/**
+	 * It creates an sluggified version from the name of the category.
+	 */
+	slug() {
+		return this.name
+			.split(' ')
+			.map(slice => slice.toLowerCase())
+			.join('-');
+	}
+
 	addSource(source) {
 		this.sources.push(source);
 	}
 
 	render() {
-		let markup = `<li>${this.name}</li>`;
+		let markup = `<li><a class="category__link" href="#/category/${this.slug()}">${this.name}</a></li>`;
 		return markup;
+	}
+
+	static loadCategory(e) {
+		e.preventDefault();
+		let name = e.target.textContent;
+		Category.openCategory(name);
+		Sidebar.get().close();
+		Router.go(`${name} - ${window.app.name}`, e.target.href);
+	}
+
+	static openCategory(name) {
+		let category = window.db.category(name);
+
+		// fetch all posts by source
+		let posts = [];
+		for (let source of category.sources) {
+			posts = [...posts, ...window.db.postsBySource(source)];
+		}
+		Post.render(posts);
+		document.querySelector('.current-section').textContent = `Category: ${name}`;
+
+		// change app state
+		window.app.state = 'category';
+	}
+
+	static addListeners() {
+		let category_links = document.querySelectorAll('.category__link');
+		category_links.forEach(link => {
+			link.addEventListener('click', Category.loadCategory, false);
+		});
 	}
 
 	static render(categories) {
@@ -34,5 +78,6 @@ export default class Category {
 		categories.forEach(category => fragment.push(category.render()));
 		let $categories = document.querySelector('.categories');
 		$categories.innerHTML = `<ul>${fragment.join('')}</ul>`;
+		Category.addListeners();
 	}
 }
