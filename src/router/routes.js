d7eb525807b7cdffbf4d5d46081eb05eb1313035
Luis Adame <mrluissan@gmail.com>
2018-11-16 23:13:30 +0100

    Add category route to routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,19 @@
 import Post from '../post';
+import Category from '../category';
 
 export default [{
 	url: '^/post/([a-z-]+)$',
 	handler: Post.openPost
 },
 {
+	url: '^/category/([a-z-]+)$',
+	handler: Category.openCategory
+},
+{
 	url: '^/example$',
 	handler: () => {
 		//eslint-disable-next-line
-            console.log('example');
+			console.log('example');
 	}
 }
 ];
