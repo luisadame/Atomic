dd65be5d2a1ea6fc35d34df23be02df38660e3eb
Luis Adame <mrluissan@gmail.com>
2018-11-18 22:06:44 +0100

    Implement font awesome with tree shaking

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill';
 import '../assets/css/app.scss';
 import '../assets/img/logo.svg';
+import './plugins/fontawesome';
 
 // database
 import repository from './repository.js';
@@ -9,17 +10,20 @@ import Router from './router';
 import routes from './router/routes';
 import Post from './post';
 import Sidebar from './components/sidebar';
+import Search from './components/search';
 import Category from './category';
 import config from './config';
 const db = new Database(repository);
 window.db = db;
 window.app = config;
+
 //eslint-disable-next-line no-console
 Post.render(db.posts);
-Category.render(db.categories);
-
-// we'll split this later
-Sidebar.listen();
 
 // Listen url changes
 Router.listen(routes);
+
+// we'll split this later
+Category.render(db.categories);
+Sidebar.listen();
+Search.listen();
---
+++ src/plugins/fontawesome.js
@@ -0,0 +1,13 @@
+import {
+	dom,
+	library
+} from '@fortawesome/fontawesome-svg-core';
+import {
+	faPlus,
+	faSearch,
+	faEllipsisV
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlus, faSearch, faEllipsisV);
+
+dom.i2svg();
