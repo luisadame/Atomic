7a54343439579ea580a4f72c650ad27804dcea10
Luis Adame <mrluissan@gmail.com>
2018-12-01 01:08:23 +0100

    Change database file to use pouch db

--- src/database.js
+++ src/database.js
@@ -1,18 +1,63 @@
+import PouchDB from 'pouchdb';
+import find from 'pouchdb-find';
+import {
+	desluggify
+} from '../src/utils';
+
 export default class Database {
 	constructor(repository) {
 		this.repository = repository;
+		this.driver = PouchDB;
+		const models = ['posts', 'categories', 'sources'];
+		PouchDB.plugin(find);
+		models.forEach(model => {
+			this[model] = new PouchDB(model);
+		});
+		this.posts.createIndex({
+			index: {
+				fields: ['title']
+			}
+		});
+	}
+
+	set db(db) {
+		if (!(db instanceof PouchDB)) throw new Error('Db has to be an instance of PouchDB');
+		this._db = db;
+	}
+
+	get db() {
+		return this._db;
+	}
+
+	validateDb(db) {
+		if (!(db instanceof this.driver)) throw new Error(`Database has to be an instance of ${this.driver}`);
+	}
+
+	set posts(db) {
+		this.validateDb(db);
+		this._posts = db;
 	}
 
 	get posts() {
-		return this.repository.posts;
+		return this._posts;
+	}
+
+	set sources(db) {
+		this.validateDb(db);
+		this._sources = db;
+	}
+
+	get sources() {
+		return this._sources;
+	}
+
+	set categories(db) {
+		this.validateDb(db);
+		this._categories = db;
 	}
 
 	get categories() {
 		return this.repository.categories;
-	}
-
-	get sources() {
-		return this.repository.sources;
 	}
 
 	category(name) {
@@ -24,7 +69,13 @@ export default class Database {
 	}
 
 	post(name) {
-		return this.posts.find(post => post.title === name);
+		return this.posts.find({
+			selector: {
+				title: name
+			}
+		}).then(result => {
+			return result.docs[0];
+		});
 	}
 
 	searchPosts(title) {
@@ -37,9 +88,25 @@ export default class Database {
 	}
 
 	postBySlug(slug) {
-		return this.posts.find(post => post.slug() === slug);
+		let name = desluggify(slug);
+		return this.posts.find({
+			selector: {
+				title: name
+			}
+		}).then(result => {
+			return result.docs[0];
+		});
 	}
 	postsBySource(source) {
 		return this.posts.filter(post => post.source === source);
+	}
+
+	static async save(data, database) {
+		try {
+			return await this.db[database].bulkDocs(data);
+		} catch (e) {
+			// eslint-disable-next-line no-console
+			console.error(e);
+		}
 	}
 }
