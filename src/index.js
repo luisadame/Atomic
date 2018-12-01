3eb050010159cf06308d5721622e974eed824225
Luis Adame <mrluissan@gmail.com>
2018-12-01 01:09:03 +0100

    Change entry point, using pouch db

--- src/index.js
+++ src/index.js
@@ -14,18 +14,50 @@ import Search from './components/search';
 import Category from './category';
 import config from './config';
 import Source from './source';
+import Parser from './parser';
 const db = new Database(repository);
 window.db = db;
 window.app = config;
 
-//eslint-disable-next-line no-console
-Post.render(db.posts);
+async function init() {
+	let sources;
+	try {
+		sources = await db.sources.allDocs({
+			include_docs: true
+		});
+		sources = sources.rows.map(row => row.doc);
+		Source.render(sources.map(Source.fromObject));
+	} catch (e) {
+		// eslint-disable-next-line no-console
+		console.error(e);
+		if (!sources.length) {
+			Post.render([]);
+			return;
+		}
+	}
+	let source = sources[0];
+	let data = await fetch(config.proxy + source.url, {
+		mode: 'cors',
+	});
+	data = await data.text();
+	let parser = new Parser({
+		data: data,
+		source: source
+	});
+	let posts = parser.posts();
+	db.posts.bulkDocs(posts.map(post => post.toObject())).then(result => {
+		Post.render(posts).catch(e => console.error(e));
+	}).catch(e => {
+		console.error(e);
+	});
+}
+
+init();
 
 // Listen url changes
 Router.listen(routes);
 
 // we'll split this later
-Category.render(db.categories);
-Source.render(db.sources);
+// Category.render(db.categories);
 Sidebar.listen();
 Search.listen();
