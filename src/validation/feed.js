8eacf5982a9f3903291b737c6fc3a1a422696972
Luis Adame <mrluissan@gmail.com>
2018-12-04 20:41:32 +0100

    Add feed validator

--- src/validation/feed.js
+++ src/validation/feed.js
@@ -2,47 +2,47 @@ import config from '../config';
 
 export default class Feed {
 
-    static async fetch(url) {
-        return fetch(url).then(response => response.text());
-    }
+	static async fetch(url) {
+		return fetch(url).then(response => response.text());
+	}
 
-    static isXML(data) {
-        return data.substr(0,5) === '<?xml';
-    }
+	static isXML(data) {
+		return data.substr(0, 5) === '<?xml';
+	}
 
-    static async containsFeedUrl(html) {
-        let dom = new DOMParser().parseFromString(html, 'text/html');
-        let feed = dom.head.querySelector('link[rel="alternate"]');
-        return feed ? Promise.resolve(feed.href) : Promise.reject();
-    }
+	static async containsFeedUrl(html) {
+		let dom = new DOMParser().parseFromString(html, 'text/html');
+		let feed = dom.head.querySelector('link[rel="alternate"]');
+		return feed ? Promise.resolve(feed.href) : Promise.reject();
+	}
 
-    static async validate(url) {
-        this.fetch(config.proxy + url)
-            .then(data => {
-                if(this.isXML(data)) {
-                    Promise.resolve(data);
-                } else {
-                    this.containsFeedUrl(data)
-                        .then(url => {
-                            this.fetch(url)
-                                .then(data => {
-                                    if(this.isXML(data)) {
-                                        Promise.resolve(data);
-                                    } else {
-                                        Promise.reject("The feed type is not compatible.");
-                                    }
-                                })
-                                .catch(() => {
-                                    throw new Error('Couldn\'t fetch the feed');
-                                });
-                        })
-                        .catch(() => {
-                            throw new Error('The feed type is not compatible.');
-                        })
-                }
-            })
-            .catch(error => {
-                throw new Error('Could\'t fetch the feed.');
-            })
-    }
+	static async validate(url) {
+		return this.fetch(config.proxy + url)
+			.then(data => {
+				if (this.isXML(data)) {
+					return data;
+				} else {
+					this.containsFeedUrl(data)
+						.then(url => {
+							this.fetch(url)
+								.then(data => {
+									if (this.isXML(data)) {
+										return data;
+									} else {
+										Promise.reject('The feed type is not compatible.');
+									}
+								})
+								.catch(() => {
+									throw new Error('Couldn\'t fetch the feed');
+								});
+						})
+						.catch(() => {
+							throw new Error('The feed type is not compatible.');
+						});
+				}
+			})
+			.catch(() => {
+				throw new Error('Could\'t fetch the feed.');
+			});
+	}
 }
