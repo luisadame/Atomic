99b86a29f82388d891aa7cfc014e558c8b5fc4b5
Luis Adame <mrluissan@gmail.com>
2018-12-04 14:10:58 +0100

    Add more steps into source addition

--- src/components/source-add-modal.js
+++ src/components/source-add-modal.js
@@ -1,51 +1,80 @@
-import Modal from "./modal";
+import Modal from './modal';
+import {
+	debounce
+} from '../utils';
+import FeedValidator from '../validation/feed';
 
 export default class SourceModal extends Modal {
 
-    proceed() {
-        let $sourceInput = document.getElementById('source-url');
-        if($sourceInput.checkValidity()) {
-            // inspect the url for rss
+	async getFeedInfo(xml) {
+		try {
+			const dom = new DOMParser().parseFromString(xml, 'text/xml');
+			let info = {
+				title: dom.querySelector('title'),
+				description: dom.querySelector('description')
+			};
+			Promise.resolve(info);
+		} catch(error) {
+			Promise.reject(error);
+		}
+	}
 
-        } else {
-            alert('show errors');
-        }
-    }
+	proceed() {
+		let $sourceInput = document.getElementById('source-url');
+		if($sourceInput.checkValidity()) {
 
-    static listen() {
-        let $addBtn = document.querySelector('.js-add-source');
-        $addBtn.addEventListener('click', SourceModal.toggle);
-    }
+			FeedValidator.validate($sourceInput.value)
+				.then(data => {
+					this.getFeedInfo(data)
+						.then()
+				})
+		} else {
+			alert('show errors');
+		}
+	}
 
-    static toggle() {
-        if (Modal.instance) {
-            Modal.instance.close();
-        } else {
-            SourceModal.open();
-        }
-    }
+	open() {
+		super.open();
+		let $sourceInput = document.getElementById('source-url');
+		$sourceInput.addEventListener('input', debounce(this.displaySourceInfo, 200));
+	}
 
-    static open() {
-        let markup = `
+	static listen() {
+		let $addBtn = document.querySelector('.js-add-source');
+		$addBtn.addEventListener('click', SourceModal.toggle);
+	}
+
+	static toggle() {
+		if (Modal.instance) {
+			Modal.instance.close();
+		} else {
+			SourceModal.open();
+		}
+	}
+
+	static open() {
+		let markup = `
             <header><h2>Add a source</h2></header>
             <div class="container">
                 <div class="input-group">
                     <label>
                         Source of news
                     </label>
-                    <input required id="source-url" type="url">
+					<input required id="source-url" type="url">
+					<div class="loader"></div>
                 </div>
-            </div>
+			</div>
+			<div class="feed-info"></div>
             <div class="submit">
                 <button class="js-ok modal__btn">Add</button>
                 <button class="js-cancel modal__btn modal__btn--link">Cancel</button>
             </div>
         `;
-        let modal = new this(markup);
-        modal.open();
-    }
+		let modal = new this(markup);
+		modal.open();
+	}
 
-    static close() {
-        Modal.instance.close();
-    }
+	static close() {
+		Modal.instance.close();
+	}
 }
