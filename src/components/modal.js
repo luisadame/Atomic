09635c55dfa2e47bf04bf8cc924ea1e41836b0cd
Luis Adame <mrluissan@gmail.com>
2018-12-04 20:42:31 +0100

    Add modal parent component

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,47 +1,47 @@
 export default class Modal {
-    constructor(html) {
-        this.cloak = document.querySelector('.sidebar__cloak');
-        this.content = html;
-    }
+	constructor(html) {
+		this.cloak = document.querySelector('.sidebar__cloak');
+		this.content = html;
+	}
 
-    addlisteners() {
-        this.$ok = document.querySelector('.modal__btn.js-ok');
-        this.$cancel = document.querySelector('.modal__btn.js-cancel');
-        this.$cancel.addEventListener('click', this.close);
-        this.$ok.addEventListener('click', this.proceed);
-    }
+	addlisteners() {
+		this.$ok = document.querySelector('.modal__btn.js-ok');
+		this.$cancel = document.querySelector('.modal__btn.js-cancel');
+		this.$cancel.addEventListener('click', this.close);
+		this.$ok.addEventListener('click', this.proceed.bind(this));
+	}
 
-    proceed() {
+	proceed() {
 
-    }
+	}
 
-    static set instance(object) {
-        // if(!(object instanceof Modal) || object !== null) throw new Error('Object given should be an instance of Modal');
-        this._instance = object;
-    }
+	static set instance(object) {
+		// if(!(object instanceof Modal) || object !== null) throw new Error('Object given should be an instance of Modal');
+		this._instance = object;
+	}
 
-    static get instance() {
-        return this._instance;
-    }
+	static get instance() {
+		return this._instance;
+	}
 
-    wrapper(html) {
-        return `<div id="modal">${html}</div>`;
-    }
+	wrapper(html) {
+		return `<div id="modal">${html}</div>`;
+	}
 
-    open() {
-        if (Modal.instance) return;
-        this.cloak.classList.toggle('open')
-        let markup = this.wrapper(this.content);
-        document.body.insertAdjacentHTML('beforeend', markup);
-        Modal.instance = this;
-        this.addlisteners();
-    }
+	open() {
+		if (Modal.instance) return;
+		this.cloak.classList.toggle('open');
+		let markup = this.wrapper(this.content);
+		document.body.insertAdjacentHTML('beforeend', markup);
+		Modal.instance = this;
+		this.addlisteners();
+	}
 
-    close() {
-        // get modal
-        let modal = document.getElementById('modal');
-        // close it
-        modal.remove();
-        Modal.instance = null;
-    }
+	close() {
+		// get modal
+		let modal = document.getElementById('modal');
+		// close it
+		modal.remove();
+		Modal.instance = null;
+	}
 }
