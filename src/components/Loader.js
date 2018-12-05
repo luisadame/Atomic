2e76fd489ec67226baea713c71a4cc815a300d5c
Luis Adame <mrluissan@gmail.com>
2018-12-05 21:24:31 +0100

    Add loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,3 +1,18 @@
 export default class Loader {
+	static toggle() {
+		let loader = document.getElementById('main-loader');
+		if (loader) {
+			loader.classList.toggle('loader--show');
+		} else {
+			Loader.init();
+			Loader.toggle();
+		}
+	}
 
+	static init() {
+		if (!document.getElementById('main-loader')) {
+			let markup = '<div id="main-loader"><div class="loader">Loading...</div></div>';
+			document.body.insertAdjacentHTML('beforeend', markup);
+		}
+	}
 }
