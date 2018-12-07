9baaaf8b7e2b9a6d67c638ec095b06b69bc016fa
Luis Adame <mrluissan@gmail.com>
2018-12-07 20:56:48 +0100

    Add update method

--- src/model.js
+++ src/model.js
@@ -10,6 +10,19 @@ export default class Model {
 		return obj;
 	}
 
+	async update() {
+		try {
+			let doc = await window.db[this._database].get(this._id);
+			return await window.db[this._database].put({
+				_rev: doc._rev,
+				...this.toObject()
+			});
+		} catch (e) {
+			// eslint-disable-next-line no-console
+			console.error(e);
+		}
+	}
+
 	async save() {
 		try {
 			return await window.db[this._database].put(this.toObject());
