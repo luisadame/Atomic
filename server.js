cc8c4676fbf7bcf78563cbf4b5976cc735be83d9
Luis Adame <mrluissan@gmail.com>
2018-12-08 17:05:01 +0100

    Change default port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const convert = require('xml-js');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 let options = {
 	origin: '*'
 };
