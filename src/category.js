69291581e555ba1c2ef887f224701df5d88ccdf9
Luis Adame <mrluissan@gmail.com>
2018-12-09 22:37:29 +0100

    Fix the entire category entity

--- src/category.js
+++ src/category.js
@@ -1,11 +1,29 @@
 import Router from './router';
 import Post from './post';
 import Sidebar from './components/sidebar';
+import Model from './model';
+import Source from './source';
+import { desluggify } from './utils';
 
-export default class Category {
+export default class Category extends Model {
 	constructor(name) {
+		super();
 		this.name = name;
 		this.sources = [];
+		this.attributes = ['_id', 'name', 'sources'];
+		this._database = 'categories';
+	}
+
+	get _id() {
+		return this.name;
+	}
+
+	static get attributes() {
+		return {
+			_id: 'string',
+			name: 'string',
+			sources: 'array'
+		};
 	}
 
 	set name(name) {
@@ -34,18 +52,39 @@ export default class Category {
 			.join('-');
 	}
 
+	removeSource(source) {
+		if (!source.url) return;
+		let found = this.sources.findIndex(source => source.url === source.url);
+		this.sources.splice(found, 1);
+		this.update();
+	}
+
 	addSource(source) {
 		this.sources.push(source);
+		this.update();
 	}
 
 	render() {
 		let markup = `<li>
 			<a class="category__link" href="#/category/${this.slug()}">${this.name}</a>
 			<ul>
-				${this.sources.map(source => '<li>' + source.render() + '</li>').join('')}
+				${this.sources.map(source => '<li>' + Source.fromObject(source).render() + '</li>').join('')}
 			</ul>
 		</li>`;
 		return markup;
+	}
+
+	isUnique() {
+		return window.db[this._database].get(this._id, (_, doc) => {
+			return !!doc;
+		});
+	}
+
+	static fromObject(object) {
+		if (!object.name ||!object.sources) return;
+		let category = new Category(object.name);
+		category.sources = object.sources;
+		return category;
 	}
 
 	static loadCategory(e) {
@@ -56,19 +95,29 @@ export default class Category {
 		Router.go(`${name} - ${window.app.name}`, e.target.href);
 	}
 
-	static openCategory(name) {
-		let category = window.db.category(name);
+	static openCategoryBySlug(slug) {
+		Category.openCategory(desluggify(slug));
+	}
+
+	static async renderCategoryPosts(sources) {
+		let posts = [];
+		for (let source of sources) {
+			posts = [...posts, ...await window.db.postsBySource(source)];
+		}
+		posts = posts.sort(Post.sortByDate);
+		Post.render(posts);
+	}
+
+	static async openCategory(name) {
+		let category = await window.db.category(name);
 
 		// fetch all posts by source
-		let posts = [];
-		for (let source of category.sources) {
-			posts = [...posts, ...window.db.postsBySource(source)];
-		}
-		Post.render(posts);
+		await this.renderCategoryPosts(category.sources);
 		document.querySelector('.current-section').textContent = `Category: ${name}`;
 
 		// change app state
 		window.app.state = 'category';
+		window.app.category = Category.fromObject(category);
 	}
 
 	static addListeners() {
@@ -88,5 +137,20 @@ export default class Category {
 		} else {
 			$categories.innerHTML = '<div class="emptiness">No categories yet :)</div>';
 		}
+	}
+
+	static all() {
+		return window.db.categories.allDocs({include_docs: true})
+			.then(result => {
+				let categories = result.rows.filter(row => row.doc.name)
+					.map(row => Category.fromObject(row.doc))
+					.sort((a, b) => {
+						return a.name - b.name;
+					});
+				return categories;
+			})
+			.catch(e => {
+				throw new Error(e);
+			});
 	}
 }
