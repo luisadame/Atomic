d75ef1380d1ad86e2042b059aaca6a2867e8107d
Luis Adame <mrluissan@gmail.com>
2018-12-16 21:50:56 +0100

    Add options component

---
+++ src/components/options.js
@@ -0,0 +1,94 @@
+import Source from '../source';
+
+export default class Options {
+
+	constructor() {
+		this.$options = document.querySelector('.options');
+		this.opened = false;
+		this.events = [];
+	}
+
+	static set markup(html) {
+		Options._markup = html;
+	}
+
+	static get markup() {
+		return Options._markup;
+	}
+
+	static get instance() {
+		if (Options._instance) {
+			return Options._instance;
+		} else {
+			Options.instance = new Options();
+			return Options._instance;
+		}
+	}
+
+	static set instance(instance) {
+		Options._instance = instance;
+	}
+
+	static source() {
+		let markup = `
+            <ul>
+                <li>
+                    <button class="btn js-delete-source">Delete source</button>
+                </li>
+            </ul>
+        `;
+		Options.instance.markup = markup;
+	}
+
+	close() {
+		this.$options.classList.remove('open');
+		this.opened = false;
+	}
+
+	open() {
+		this.$options.classList.add('open');
+		this.opened = true;
+	}
+
+	static toggle() {
+		if (Options.instance.opened) {
+			Options.instance.close();
+		} else {
+			Options.instance.open();
+		}
+	}
+
+	static async render() {
+		let instance = Options.instance;
+		instance.$options.innerHTML = instance.markup;
+	}
+
+	static addListeners() {
+		let instance = Options.instance;
+		instance.events = [{
+			el: document.querySelector('.js-delete-source'),
+			event: 'click',
+			handler: Source.delete
+		}];
+		for (let event of instance.events) {
+			event.el.addEventListener(event.event, event.handler);
+		}
+	}
+
+	static init(e) {
+		e.preventDefault();
+		switch(window.app.state) {
+		case 'source':
+			Options.source();
+			break;
+		}
+		Options.render()
+			.then(Options.addListeners)
+			.then(Options.toggle);
+	}
+
+	static listen() {
+		let $options = document.querySelector('.js-options');
+		$options.addEventListener('click', Options.init.bind(Options));
+	}
+}
