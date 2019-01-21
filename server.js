23ffc9ddbcdac16471b9e71109b8f6062b8d7aed
Luis Adame <mrluissan@gmail.com>
2019-01-21 20:19:18 +0100

    Set origins on cors

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const convert = require('xml-js');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+const allowedOrigins = ['https://atomic-app.netlify.com', 'localhost'];
 let options = {
-	origin: '*'
+	origin: function(origin, callback) {
+		if (allowedOrigins.includes(origin)) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	}
 };
 app.get('/', cors(options), async (req, res) => {
 	let url = req.query.url;
