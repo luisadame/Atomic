965c3b0874f74a3514c3244fcd944b2112bdd43a
Luis Adame <mrluissan@gmail.com>
2019-02-04 22:08:47 +0100

    wip

--- src/config.js
+++ src/config.js
@@ -1,5 +1,11 @@
 export default {
 	name: 'Atomic',
 	state: 'home',
-	proxy: 'http://atomic-api.test/api/proxy/'
+	proxy: 'http://atomic-api.test/api/proxy/',
+	fetchOptions: {
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}
 };
