52f13df5860f5b3f3563a2fe72e8eb231ec19d31
Luis Adame <mrluissan@gmail.com>
2019-02-11 13:54:24 +0100

    Fix validator error getter

--- src/validation/Validator/index.js
+++ src/validation/Validator/index.js
@@ -1,73 +1,73 @@
-
+import methods from './methods';
 export default class Validator {
-    constructor(rules, form) {
-        this.rules = rules;
-        this.form = form;
-        this.errors = null;
-    }
+	constructor(rules, form) {
+		this.rules = rules;
+		this.form = form;
+		this.errors = null;
+	}
 
-    get rules() {
-        return this._rules;
-    }
+	get rules() {
+		return this._rules;
+	}
 
-    set rules(rules) {
-        if (typeof rules !== 'object') {
-            throw new Error('Rules must be an object with rules');
-        }
-        this._rules = rules;
-    }
+	set rules(rules) {
+		if (typeof rules !== 'object') {
+			throw new Error('Rules must be an object with rules');
+		}
+		this._rules = rules;
+	}
 
-    get form() {
-        return this._form;
-    }
+	get form() {
+		return this._form;
+	}
 
-    set form(form) {
-        if (!(form instanceof HTMLFormElement)) {
-            throw new Error('Form must be a form element');
-        }
-        this._form = form;
-    }
+	set form(form) {
+		if (!(form instanceof HTMLFormElement)) {
+			throw new Error('Form must be a form element');
+		}
+		this._form = form;
+	}
 
-    buildValidations(validations) {
-        validations = validations.split('|');
-        return validations.map(validation => {
-            if (validation.includes(':')) {
-                [validation, param] = validation.split(':');
-            }
-            return {
-                name: validation,
-                param: param ? param : null
-            };
-        })
-    }
+	buildValidations(validations) {
+		validations = validations.split('|');
+		return validations.map(validation => {
+			if (validation.includes(':')) {
+				[validation, param] = validation.split(':');
+			}
+			return {
+				name: validation,
+				param: param ? param : null
+			};
+		});
+	}
 
-    make() {
-        for (let name in this.rules) {
-            let value = this.form.querySelector(`[name=${name}]`).value;
-            if (!value) throw new Error(`Element ${name} is not within the form.`);
-            for (let rule of this.buildValidations(this.rules[name])) {
-                if (rule.param) {
-                    if (!this[rule.name](value, rule.param)) {
-                        this.errors[name] = this.message(rule.name, rule.param);
-                    }
-                } else {
-                    if (!this[rule.name](value)) {
-                        this.errors[name] = this.message(rule.name);
-                    }
-                }
-            }
-        }
-    }
+	make() {
+		for (let name in this.rules) {
+			let value = this.form.querySelector(`[name=${name}]`).value;
+			if (!value) throw new Error(`Element ${name} is not within the form.`);
+			for (let rule of this.buildValidations(this.rules[name])) {
+				if (rule.param) {
+					if (!this[rule.name](value, rule.param)) {
+						this.errors[name] = this.message(rule.name, rule.param);
+					}
+				} else {
+					if (!this[rule.name](value)) {
+						this.errors[name] = this.message(rule.name);
+					}
+				}
+			}
+		}
+	}
 
-    message(name, param = null) {
-        return
-    }
+	message(name, param = null) {
+		return methods[name].msg(param);
+	}
 
-    isValid() {
-        return this.messages === null;
-    }
+	isValid() {
+		return this.errors === null;
+	}
 
-    messages() {
-        return this.messages;
-    }
+	errors() {
+		return this.errors;
+	}
 }
--- src/validation/Validator/methods.js
+++ src/validation/Validator/methods.js
@@ -1,29 +1,35 @@
 export default {
-    required: {
-        fn: function(value) {
-            return value.length > 0;
-        },
-        msg: 'This field is required'
-    },
-    string: {
-        fn: function(value) {
-            return typeof value !== 'string';
-        },
-        msg: 'This field must be a string'
-    },
-    email: {
-        fn: function(value) {
-            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return !re.test(String(value).toLowerCase());
-        },
-        msg: 'This field must be a valid email'
-    },
-    match: {
-        fn: function(value, param) {
-            let fieldToMatch = document.querySelector(`[name="${param}"]`);
-            return value !== fieldToMatch.value;
-        },
-        msg: 'This field'
-    }
+	required: {
+		fn: function(value) {
+			return value.length > 0;
+		},
+		msg: 'This field is required'
+	},
+	string: {
+		fn: function(value) {
+			return typeof value !== 'string';
+		},
+		msg: function() {
+			return 'This field must be a string';
+		}
+	},
+	email: {
+		fn: function(value) {
+			var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return !re.test(String(value).toLowerCase());
+		},
+		msg: function() {
+			return 'This field must be a valid email';
+		}
+	},
+	match: {
+		fn: function(value, param) {
+			let fieldToMatch = document.querySelector(`[name="${param}"]`);
+			return value !== fieldToMatch.value;
+		},
+		msg: function(field) {
+			return `This field must match against ${field} field`;
+		}
+	}
 
-}
+};
