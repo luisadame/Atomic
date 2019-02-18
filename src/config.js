e58178c27605050d6f897e01aa122d8a63651c48
Luis Adame <mrluissan@gmail.com>
2019-02-18 14:21:27 +0100

    Convert config to proxy

--- src/config.js
+++ src/config.js
@@ -1,13 +1,36 @@
-export default {
+import Auth from './auth';
+
+let handler = {
+	set: function (obj, prop, value) {
+		obj[prop] = value;
+		if (prop === 'authenticated') {
+			if (value) {
+				Auth.afterLogin();
+			}
+		}
+		return true;
+	}
+};
+
+let config = {
 	name: 'Atomic',
 	state: 'home',
 	backend: 'http://atomic-api.test/api',
 	proxy: 'http://atomic-api.test/api/proxy/',
 	authenticated: false,
-	fetchOptions: {
-		mode: 'cors',
-		headers: {
-			'Content-Type': 'application/json'
+	fetchOptions: () => {
+		let access_token = localStorage.getItem('access_token');
+		let options = {
+			mode: 'cors',
+			headers: {
+				'Accept': 'application/json, text/plain, */*'
+			}
+		};
+		if (access_token) {
+			options.headers['Authorization'] = `Bearer ${access_token}`;
 		}
+		return options;
 	}
 };
+
+export default new Proxy(config, handler);
